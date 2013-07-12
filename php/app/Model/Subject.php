8ea783e43c5707e604d12d192524f3805ae332b9
Sameera Hokandara <cham.lakmal@gmail.com>
2013-07-12 18:23:34 +0530

    Added the Subject model with initial basic validation

---
+++ php/app/Model/Subject.php
@@ -0,0 +1,36 @@
+<?php
+App::uses('AppModel', 'Model');
+
+class Subject extends AppModel {
+
+
+	public $displayField = 'name';
+
+
+	public $validate = array(
+		'name' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+
+			),
+		),
+	);
+
+
+	public $hasMany = array(
+		'CourseUnit' => array(
+			'className' => 'CourseUnit',
+			'foreignKey' => 'subject_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);
+
+}
