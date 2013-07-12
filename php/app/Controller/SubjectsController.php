5be30147fb0dc17af2f649e71611ccb7733b2adf
Sameera Hokandara <cham.lakmal@gmail.com>
2013-07-12 18:59:30 +0530

    Added the related initial controller to the subject model

---
+++ php/app/Controller/SubjectsController.php
@@ -0,0 +1,66 @@
+<?php
+App::uses('AppController', 'Controller');
+
+class SubjectsController extends AppController {
+
+
+	public function index() {
+		$this->Subject->recursive = 0;
+		$this->set('subjects', $this->paginate());
+	}
+
+
+	public function view($id = null) {
+		if (!$this->Subject->exists($id)) {
+			throw new NotFoundException(__('Invalid subject'));
+		}
+		$options = array('conditions' => array('Subject.' . $this->Subject->primaryKey => $id));
+		$this->set('subject', $this->Subject->find('first', $options));
+	}
+
+
+	public function add() {
+		if ($this->request->is('post')) {
+			$this->Subject->create();
+			if ($this->Subject->save($this->request->data)) {
+				$this->Session->setFlash(__('The subject has been saved'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The subject could not be saved. Please, try again.'));
+			}
+		}
+	}
+
+
+	public function edit($id = null) {
+		if (!$this->Subject->exists($id)) {
+			throw new NotFoundException(__('Invalid subject'));
+		}
+		if ($this->request->is('post') || $this->request->is('put')) {
+			if ($this->Subject->save($this->request->data)) {
+				$this->Session->setFlash(__('The subject has been saved'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The subject could not be saved. Please, try again.'));
+			}
+		} else {
+			$options = array('conditions' => array('Subject.' . $this->Subject->primaryKey => $id));
+			$this->request->data = $this->Subject->find('first', $options);
+		}
+	}
+
+
+	public function delete($id = null) {
+		$this->Subject->id = $id;
+		if (!$this->Subject->exists()) {
+			throw new NotFoundException(__('Invalid subject'));
+		}
+		$this->request->onlyAllow('post', 'delete');
+		if ($this->Subject->delete()) {
+			$this->Session->setFlash(__('Subject deleted'));
+			$this->redirect(array('action' => 'index'));
+		}
+		$this->Session->setFlash(__('Subject was not deleted'));
+		$this->redirect(array('action' => 'index'));
+	}
+}
