197d83bf3a5474e74812222bd0bcfbf058f72495
Uditha Bandara Wijerathna <udithabnd@gmail.com>
2013-07-15 00:53:16 +0530

    Initiation of developement of the messaging comp.

---
+++ php/app/Controller/MessagesController.php
@@ -0,0 +1,16 @@
+<?php
+App::uses('AppController', 'Controller');
+App::uses('CakeEmail', 'Network/Email');
+
+class MessagesController extends AppController {
+
+    public $uses = array();
+
+    public function sms() {
+
+    }
+
+    public function email() {
+
+    }
+}
---
+++ php/app/View/Messages/email.ctp
@@ -0,0 +1,3 @@
+<?php $this->layout = 'bootstrap2'; ?>
+<?php $this->set('title', 'Send an Email'); ?>
+
