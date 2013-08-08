10211bc60952d71c9b43e59e049663575abd5a94
Shayamali Dulangika <cuteshami12@gmail.com>
2013-08-08 15:11:16 +0530

    Added Organizations controller with CRUD actions

---
+++ php/app/Controller/OrganizationsController.php
@@ -0,0 +1,96 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Organizations Controller
+ *
+ * @property Organization $Organization
+ */
+class OrganizationsController extends AppController {
+
+/**
+ * index method
+ *
+ * @return void
+ */
+	public function index() {
+		$this->Organization->recursive = 0;
+		$this->set('organizations', $this->paginate());
+	}
+
+/**
+ * view method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function view($id = null) {
+		if (!$this->Organization->exists($id)) {
+			throw new NotFoundException(__('Invalid organization'));
+		}
+		$options = array('conditions' => array('Organization.' . $this->Organization->primaryKey => $id));
+		$this->set('organization', $this->Organization->find('first', $options));
+	}
+
+/**
+ * add method
+ *
+ * @return void
+ */
+	public function add() {
+		if ($this->request->is('post')) {
+			$this->Organization->create();
+			if ($this->Organization->save($this->request->data)) {
+				$this->Session->setFlash(__('The organization has been saved'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The organization could not be saved. Please, try again.'));
+			}
+		}
+	}
+
+/**
+ * edit method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function edit($id = null) {
+		if (!$this->Organization->exists($id)) {
+			throw new NotFoundException(__('Invalid organization'));
+		}
+		if ($this->request->is('post') || $this->request->is('put')) {
+			if ($this->Organization->save($this->request->data)) {
+				$this->Session->setFlash(__('The organization has been saved'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The organization could not be saved. Please, try again.'));
+			}
+		} else {
+			$options = array('conditions' => array('Organization.' . $this->Organization->primaryKey => $id));
+			$this->request->data = $this->Organization->find('first', $options);
+		}
+	}
+
+/**
+ * delete method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function delete($id = null) {
+		$this->Organization->id = $id;
+		if (!$this->Organization->exists()) {
+			throw new NotFoundException(__('Invalid organization'));
+		}
+		$this->request->onlyAllow('post', 'delete');
+		if ($this->Organization->delete()) {
+			$this->Session->setFlash(__('Organization deleted'));
+			$this->redirect(array('action' => 'index'));
+		}
+		$this->Session->setFlash(__('Organization was not deleted'));
+		$this->redirect(array('action' => 'index'));
+	}
+}
