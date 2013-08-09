e91aecf680b24fc9fe50f3f44f3111fdb83fd294
Ishani Gunawardhana <ishanigunawardhana@gmail.com>
2013-08-09 16:41:39 +0530

    Change the email, studentMail, industryMai, and staffMail actions

--- php/app/Controller/MessagesController.php
+++ php/app/Controller/MessagesController.php
@@ -13,6 +13,10 @@ class MessagesController extends AppController {
 
     public function email($data = null) {
 
+
+
+    }
+    public function studentMail(){
         if($this->request->is('post')){
             if($this->request->data)
             {
@@ -41,18 +45,68 @@ class MessagesController extends AppController {
             return true;
         }
 
-    }
-
-    public function studentMail(){
-
 
     }
 
     public function industryMail(){
 
+        if($this->request->is('post')){
+            if($this->request->data)
+            {
+                $data = $this->request->data;
+                $Email = new CakeEmail('gmail');
+                //$Email->config('gmail');
+                $Email->from(array('postmaster.fnp@gmail.com' => "ETHERA Postmaster"));
+                $Email->sender('ethera.rjt@gmail.com', 'ETHERA Postmaster');
+                $Email->to($data['to']);
+                $Email->subject($data['subject']);
+                $Email->send($data['body']);
+            }
+
+            $this->redirect(array('controller'=>'messages','action'=>'email'));
+        }
+
+        if(!empty($data)){
+            $Email = new CakeEmail('gmail');
+            //$Email->config('gmail');
+            $Email->from(array('postmaster.fnp@gmail.com' => "ETHERA Postmaster"));
+            $Email->sender('ethera.rjt@gmail.com', 'ETHERA Postmaster');
+            $Email->to($data['to']);
+            $Email->subject($data['subject']);
+            $Email->send($data['body']);
+
+            return true;
+        }
     }
 
     public function staffMail(){
+        if($this->request->is('post')){
+            if($this->request->data)
+            {
+                $data = $this->request->data;
+                $Email = new CakeEmail('gmail');
+                //$Email->config('gmail');
+                $Email->from(array('postmaster.fnp@gmail.com' => "ETHERA Postmaster"));
+                $Email->sender('ethera.rjt@gmail.com', 'ETHERA Postmaster');
+                $Email->to($data['to']);
+                $Email->subject($data['subject']);
+                $Email->send($data['body']);
+            }
+
+            $this->redirect(array('controller'=>'messages','action'=>'email'));
+        }
+
+        if(!empty($data)){
+            $Email = new CakeEmail('gmail');
+            //$Email->config('gmail');
+            $Email->from(array('postmaster.fnp@gmail.com' => "ETHERA Postmaster"));
+            $Email->sender('ethera.rjt@gmail.com', 'ETHERA Postmaster');
+            $Email->to($data['to']);
+            $Email->subject($data['subject']);
+            $Email->send($data['body']);
+
+            return true;
+        }
 
     }
 }
