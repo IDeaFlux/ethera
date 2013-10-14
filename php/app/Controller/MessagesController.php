460a83537a64eac3adc170eddb60295354756248
Ishani Gunawardhana <ishanigunawardhana@gmail.com>
2013-10-14 22:13:08 +0530

    Enhanced sms component

--- php/app/Controller/MessagesController.php
+++ php/app/Controller/MessagesController.php
@@ -1,12 +1,14 @@
 <?php
 App::uses('AppController', 'Controller');
 App::uses('CakeEmail', 'Network/Email');
+App::import('Vendor','Dialog/SmsReceiver');
+App::import('Vendor','Dialog/SmsSender');
 
 class MessagesController extends AppController {
 
     public $uses = array();
     public $name = 'Messages';
-
+    public $components = array('RequestHandler');
 
 
     public function sms() {
@@ -153,5 +155,70 @@ class MessagesController extends AppController {
             return true;
         }
 
+    }
+
+    public function sms_receive(){
+        $this->autoRender = false;
+        try{
+            $this->RequestHandler->addInputType('json', array('json_decode', false));
+            $data = $this->request->input();
+
+            if($this->request->is('post')){
+                $receiver = new SmsReceiver($data);
+            }
+
+            $content = $receiver->getMessage(); // get the message content
+            $address = $receiver->getAddress(); // get the sender's address
+            $requestId = $receiver->getRequestID(); // get the request ID
+            $applicationId = $receiver->getApplicationId(); // get application ID
+            $encoding = $receiver->getEncoding(); // get the encoding value
+            $version = $receiver->getVersion(); // get the version
+           
+            CakeLog::write('notice',var_dump(json_decode($data, true)));
+            $split = explode('/', $content);
+
+            $responseMsg = bmiLogicHere($split,$address);
+
+            $sender = new SmsSender("https://localhost:7443/sms/send");
+
+            $applicationId = "APP_000001";
+            $encoding = "0";
+            $version =  "1.0";
+            $password = "password";
+            $sourceAddress = "77000";
+            $deliveryStatusRequest = "1";
+            $charging_amount = ":15.75";
+            $destinationAddresses = array("tel:94771122336");
+            $binary_header = "";
+
+            $res = $sender->sms($responseMsg, $destinationAddresses, $password, $applicationId, $sourceAddress, $deliveryStatusRequest, $charging_amount, $encoding, $version, $binary_header);
+        }
+
+        catch (SmsException $ex){
+
+        }
+    }
+
+    public function bmiLogicHere($split,$number)
+
+    {
+
+        if (sizeof($split) < 4) {
+            $responseMsg = "Invalid message content";
+        }
+
+        else {
+
+            $responseMsg ="Successfully registered. Your number is : ".$number;
+        }
+
+        return $responseMsg;
+    }
+
+    public function beforeFilter(){
+        parent::beforeFilter();
+        if ($this->RequestHandler->accepts('json')) {
+        }
+        $this->Auth->allow('sms_receive');
     }
 }
