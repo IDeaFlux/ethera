4efb0d2f7fe120d32c98a6e563ce78cc5e1c90ef
Shayamali Dulangika <cuteshami12@gmail.com>
2013-10-14 14:26:30 +0530

    Create intial Oppotuny model

---
+++ php/app/Model/Opportuny.php
@@ -0,0 +1,97 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Opportuny Model
+ *
+ * @property InterestedArea $InterestedArea
+ * @property Organization $Organization
+ * @property Batch $Batch
+ */
+class Opportuny extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'id';
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'interested_area_id' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'organization_id' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'batch_id' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'slots' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+	public $belongsTo = array(
+		'InterestedArea' => array(
+			'className' => 'InterestedArea',
+			'foreignKey' => 'interested_area_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'Organization' => array(
+			'className' => 'Organization',
+			'foreignKey' => 'organization_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'Batch' => array(
+			'className' => 'Batch',
+			'foreignKey' => 'batch_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+}
