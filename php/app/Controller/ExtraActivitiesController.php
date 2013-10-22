db71991082da5f96862b6406cf868df26a0945de
Yohani Ranasinghe <yohani.ysr@gmail.com>
2013-10-22 13:09:55 +0530

    Added the flash banners to messages in ExtraActivitiesController

--- php/app/Controller/ExtraActivitiesController.php
+++ php/app/Controller/ExtraActivitiesController.php
@@ -26,7 +26,7 @@ class ExtraActivitiesController extends AppController {
  */
 	public function view($id = null) {
 		if (!$this->ExtraActivity->exists($id)) {
-			throw new NotFoundException(__('Invalid extra activity'));
+			throw new NotFoundException(__('Invalid extra activity'),'error_flash');
 		}
 		$options = array('conditions' => array('ExtraActivity.' . $this->ExtraActivity->primaryKey => $id));
 		$this->set('extraActivity', $this->ExtraActivity->find('first', $options));
@@ -41,10 +41,10 @@ class ExtraActivitiesController extends AppController {
 		if ($this->request->is('post')) {
 			$this->ExtraActivity->create();
 			if ($this->ExtraActivity->save($this->request->data)) {
-				$this->Session->setFlash(__('The extra activity has been saved'));
+				$this->Session->setFlash(__('The extra activity has been saved'),'success_flash');
 				$this->redirect(array('action' => 'index'));
 			} else {
-				$this->Session->setFlash(__('The extra activity could not be saved. Please, try again.'));
+				$this->Session->setFlash(__('The extra activity could not be saved. Please, try again.'),'error_flash');
 			}
 		}
 	}
@@ -58,14 +58,14 @@ class ExtraActivitiesController extends AppController {
  */
 	public function edit($id = null) {
 		if (!$this->ExtraActivity->exists($id)) {
-			throw new NotFoundException(__('Invalid extra activity'));
+			throw new NotFoundException(__('Invalid extra activity'),'error_flash');
 		}
 		if ($this->request->is('post') || $this->request->is('put')) {
 			if ($this->ExtraActivity->save($this->request->data)) {
-				$this->Session->setFlash(__('The extra activity has been saved'));
+				$this->Session->setFlash(__('The extra activity has been saved'),'success_flash');
 				$this->redirect(array('action' => 'index'));
 			} else {
-				$this->Session->setFlash(__('The extra activity could not be saved. Please, try again.'));
+				$this->Session->setFlash(__('The extra activity could not be saved. Please, try again.'),'error_flash');
 			}
 		} else {
 			$options = array('conditions' => array('ExtraActivity.' . $this->ExtraActivity->primaryKey => $id));
@@ -83,14 +83,14 @@ class ExtraActivitiesController extends AppController {
 	public function delete($id = null) {
 		$this->ExtraActivity->id = $id;
 		if (!$this->ExtraActivity->exists()) {
-			throw new NotFoundException(__('Invalid extra activity'));
+			throw new NotFoundException(__('Invalid extra activity'),'error_flash');
 		}
 		$this->request->onlyAllow('post', 'delete');
 		if ($this->ExtraActivity->delete()) {
-			$this->Session->setFlash(__('Extra activity deleted'));
+			$this->Session->setFlash(__('Extra activity deleted'),'success_flash');
 			$this->redirect(array('action' => 'index'));
 		}
-		$this->Session->setFlash(__('Extra activity was not deleted'));
+		$this->Session->setFlash(__('Extra activity was not deleted'),'info_flash');
 		$this->redirect(array('action' => 'index'));
 	}
 }
