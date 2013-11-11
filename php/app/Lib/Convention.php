f67badc9e8c1708649936f46c69229ac07f1f958
Uditha Bandara Wijerathna <udithabnd@gmail.com>
2013-11-11 23:54:22 +0530

    Added all new Convention library

---
+++ php/app/Lib/Convention.php
@@ -0,0 +1,28 @@
+<?php
+class Convention{
+    public static function size($bytes, $precision = 2){
+            $kilobyte = 1024;
+            $megabyte = $kilobyte * 1024;
+            $gigabyte = $megabyte * 1024;
+            $terabyte = $gigabyte * 1024;
+
+            if (($bytes >= 0) && ($bytes < $kilobyte)) {
+                return $bytes . ' B';
+
+            } elseif (($bytes >= $kilobyte) && ($bytes < $megabyte)) {
+                return round($bytes / $kilobyte, $precision) . ' KB';
+
+            } elseif (($bytes >= $megabyte) && ($bytes < $gigabyte)) {
+                return round($bytes / $megabyte, $precision) . ' MB';
+
+            } elseif (($bytes >= $gigabyte) && ($bytes < $terabyte)) {
+                return round($bytes / $gigabyte, $precision) . ' GB';
+
+            } elseif ($bytes >= $terabyte) {
+                return round($bytes / $terabyte, $precision) . ' TB';
+            } else {
+                return $bytes . ' B';
+            }
+    }
+}
+?>
