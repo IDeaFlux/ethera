b4c15e423bd03dec8d0a56f9d237d5b21588f300
Uditha Bandara Wijerathna <udithabnd@gmail.com>
2013-11-17 19:27:02 +0530

    Backend router implemented

--- php/app/Controller/HomesController.php
+++ php/app/Controller/HomesController.php
@@ -26,6 +26,46 @@ class HomesController extends AppController {
 
     }
 
+    public function ar(){
+
+    }
+
+    public function cgu(){
+
+    }
+
+    public function organization(){
+        $user = $this->Auth->user();
+        $this->set('id',$user['id']);
+    }
+
+    public function backend_router(){
+        $user = $this->Auth->user();
+
+        if(!empty($user)){
+            if($user['group_id']==1){
+                $this->redirect(array('action' => 'admin'));
+            }
+            if($user['group_id']==2){
+                $this->redirect(array('action' => 'cgu'));
+            }
+            if($user['group_id']==5){
+                $this->redirect(array('action' => 'organization'));
+            }
+            if($user['group_id']==6){
+                $this->redirect(array('action' => 'ar'));
+            }
+            if($user['group_id']==4){
+                $this->loadModel('Student');
+                $this->Session->setFlash('You are not authorized to access that page','error_flash');
+                $this->redirect($this->Auth->logout());
+            }
+        }
+        else{
+            $this->redirect(array('action' => 'main'));
+        }
+    }
+
     public function beforeFilter(){
         parent::beforeFilter();
         $this->Auth->allow(array('main','login'));
